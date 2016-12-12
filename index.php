7decb6d2bd57b8714b019d27f481f3971171927d
Flavio S. de Lima <flavio.fatesg@yahoo.com.br>
2016-12-12 07:18:53 -0200

    Adição banco e correção formulario

--- index.php
+++ index.php
@@ -32,17 +32,17 @@
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
+            <span class="sr-only">NFS-e Goiânia</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="index.php">Project name</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="emitir-nota.html">Emitir Nota</a></li>
             <li><a href="#contact">Contact</a></li>
  
           </ul>
